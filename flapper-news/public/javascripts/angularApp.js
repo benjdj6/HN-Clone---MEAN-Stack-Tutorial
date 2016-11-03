5323c36b7d18292caa2c81cd9232a688c13ca573
benjdj6 <benjdj6@vt.edu>
2016-11-03 17:48:26 -0400

    New posts and upvotes now saved to and loaded from Mongo

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -35,6 +35,17 @@ app.factory('posts', ['$http', function($http){
             angular.copy(data, o.posts);
         });
     };
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data) {
+            o.posts.push(data);
+        });
+    };
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data) {
+                post.upvotes += 1;
+            });
+    };
     return o;
 }])
 
@@ -46,20 +57,15 @@ app.controller('MainCtrl', [
         $scope.posts = posts.posts;
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') { return; }
-            $scope.posts.push({
-                title: $scope.title, 
-                link: $scope.link, 
-                upvotes: 0,
-                comments: [
-                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
+            posts.create({
+                title: $scope.title,
+                link: $scope.link
             });
             $scope.title = '';
             $scope.link = '';
         };
         $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
+            posts.upvote(post);
         };
 }]);
 
