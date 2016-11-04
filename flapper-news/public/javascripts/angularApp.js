bcc948ee2dfc9d411bf847ea14e88f88317a21c7
benjdj6 <benjdj6@vt.edu>
2016-11-03 21:25:20 -0400

    Comment loading and upvoting implemented
    Comment loading and upvoting implemented, still cannot add comments
    through webpage.

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -19,7 +19,12 @@ function($stateProvider, $urlRouterProvider) {
     .state('posts', {
         url: '/posts/{id}',
         templateUrl: '/posts.html',
-        controller: 'PostsCtrl'
+        controller: 'PostsCtrl',
+        resolve: {
+            post: ['$stateParams', 'posts', function($stateParams, posts) {
+                return posts.get($stateParams.id);
+            }]
+        }
     });
 
   $urlRouterProvider.otherwise('home');
@@ -46,8 +51,22 @@ app.factory('posts', ['$http', function($http){
                 post.upvotes += 1;
             });
     };
+    o.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res) {
+            return res.data;
+        });
+    };
+    o.addComment = function(id, comment) {
+        return $http.post('/posts/', + id + '/comments', comment);
+    };
+    o.upvoteComment = function(post, comment) {
+        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+            .success(function(data) {
+                comment.upvotes += 1;
+            });
+    };
     return o;
-}])
+}]);
 
 app.controller('MainCtrl', [
     '$scope',
@@ -70,19 +89,23 @@ app.controller('MainCtrl', [
 }]);
 
 app.controller('PostsCtrl', [
-'$scope',
-'$stateParams',
-'posts',
-function($scope, $stateParams, posts){
-    $scope.post = posts.posts[$stateParams.id];
-    $scope.addComment = function() {
-        if($scope.body === '') { return; }
-        $scope.post.comments.push({
-            body: $scope.body,
-            author: 'user',
-            upvotes: 0
-        });
-        $scope.body='';
-    };
+    '$scope',
+    'posts',
+    'post',
+    function($scope, posts, post){
+        $scope.post = post;
+        $scope.addComment = function() {
+            if($scope.body === '') { return; }
+            posts.addComment(post._id, {
+                body: $scope.body,
+                author: 'user'
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
+            });
+            $scope.body = '';
+        };
+        $scope.incrementUpvotes = function(comment) {
+            posts.upvoteComment(post, comment);
+        };
 }]);
 
