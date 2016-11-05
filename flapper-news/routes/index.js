f1d5db774bb550f231535fd51217e1e610a542f4
benjdj6 <benjdj6@vt.edu>
2016-11-04 22:04:37 -0400

    Created registration and login endpoints

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -104,6 +107,44 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 
     res.json(comment);
   });
+});
+
+router.post('/register', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        return res.json({ token: user.generageJWT() });
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            returnnext(err);
+        }
+
+        if (user) {
+            return res.json({ token: user.generateJWT() });
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
