fff2171b0bf0c29316f9c51e61365a96034d6027
benjdj6 <benjdj6@vt.edu>
2016-11-12 16:42:26 -0500

    navbar added

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -204,5 +204,14 @@ app.controller('AuthCtrl', [
         };
 }]);
 
+app.controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth) {
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
+}]);
+
 
 
